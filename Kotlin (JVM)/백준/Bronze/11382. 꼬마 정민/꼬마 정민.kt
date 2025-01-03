cb009bb02dc8f88fa367dd53ccdfceec8fb74c33
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-01-03 17:32:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 108 ms, Memory: 13684 KB -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Bronze/11382. 꼬마 정민/꼬마 정민.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(){
+    val input = Scanner(System.`in`)
+    val a = input.nextBigInteger()
+    val b = input.nextBigInteger()
+    val c = input.nextBigInteger()
+
+    println(a+b+c)
+}
