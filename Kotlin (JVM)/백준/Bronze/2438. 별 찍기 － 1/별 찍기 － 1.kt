8322e3128800a7caa203080da50c36f5618811ac
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-01-03 21:55:28 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 16292 KB -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    sc.nextLine()
+
+    for (i in 1..n) {
+        println("*".repeat(i))
+    }
+}
