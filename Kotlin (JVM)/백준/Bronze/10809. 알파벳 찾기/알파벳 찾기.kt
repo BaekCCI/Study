a3c72effb3eaa67fe7db72ba478afb498465dae6
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-01-04 20:47:25 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 148 ms, Memory: 22780 KB -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.kt
@@ -0,0 +1,10 @@
+package org.example
+
+fun main() {
+    val input = readln()
+    val result = ('a'..'z').associate { it to -1 }.toMutableMap()
+    result.forEach {
+        result[it.key] = input.indexOf(it.key)
+    }
+    println(result.map { it.value }.joinToString(separator = " "))
+}
