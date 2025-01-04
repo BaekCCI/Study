c1a4894d62b8c5d51cc756b9fd41ff73f95ce21f
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-01-04 19:18:21 +0900

    [Bronze III] Title: 공 넣기, Time: 124 ms, Memory: 21488 KB -BaekjoonHub

--- Kotlin (JVM)/백준/Bronze/10810. 공 넣기/공 넣기.kt
+++ Kotlin (JVM)/백준/Bronze/10810. 공 넣기/공 넣기.kt
@@ -4,8 +4,8 @@ fun main() {
 
     for (i in 1..nm[1]) {
         val input = readln().split(" ").map { it.toInt() }
-        for (i in input[0]..input[1]) {
-            bag[i - 1] = input[2]
+        for (j in input[0]..input[1]) {
+            bag[j - 1] = input[2]
         }
     }
     println(bag.joinToString(separator = " "))
