d8bac67a6aa9822a4fc15878a5f208e88bc76425
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-01-06 19:41:46 +0900

    [Silver V] Title: 숫자 카드, Time: 1160 ms, Memory: 201312 KB -BaekjoonHub

--- Kotlin (JVM)/백준/Silver/10815. 숫자 카드/숫자 카드.kt
+++ Kotlin (JVM)/백준/Silver/10815. 숫자 카드/숫자 카드.kt
@@ -1,6 +1,6 @@
 fun main() {
     val n = readln().toInt()
-    val nums = readln().split(" ").map { it.toInt() }.toMutableSet()
+    val nums = readln().split(" ").map { it.toInt() }.toSet()
     val m = readln().toInt()
     val input = readln().split(" ").map { if (nums.contains(it.toInt())) 1 else 0 }
     println(input.joinToString(" "))
