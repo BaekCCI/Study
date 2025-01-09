8a6a27e62c3457699a7516ccd42e1c27ec337927
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-01-09 23:51:34 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 734.38 ms, Memory: 283 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.kt
+++ Kotlin/프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.kt
@@ -1,4 +1,5 @@
 import kotlin.math.sqrt
+
 class Solution {
     fun solution(arrayA: IntArray, arrayB: IntArray): Int {
 
@@ -11,8 +12,12 @@ class Solution {
 
         for (i in 1..sqrtA) {
             if (minA % i == 0) {
-                dividerA.add(i)
-                dividerA.add(minA / i)
+                if (arrayA.filter { it % i == 0 }.size == arrayA.size) {
+                    dividerA.add(i)
+                }
+                if (arrayA.filter { it % (minA / i) == 0 }.size == arrayA.size) {
+                    dividerA.add(minA / i)
+                }
             }
         }
 
@@ -21,29 +26,28 @@ class Solution {
 
         for (i in 1..sqrtB) {
             if (minB % i == 0) {
-                dividerB.add(i)
-                dividerB.add(minB / i)
+                if (arrayB.filter { it % i == 0 }.size == arrayB.size) {
+                    dividerB.add(i)
+                }
+                if (arrayB.filter { it % (minB / i) == 0 }.size == arrayB.size) {
+                    dividerB.add(minB / i)
+                }
             }
         }
         dividerA.forEach { divider ->
-            if(arrayA.filter { it%divider==0 }.size == arrayA.size){
-                if(arrayB.filter { it%divider!=0 }.size==arrayB.size){
-                    if(answer<divider){
-                        answer = divider
-                    }
+            if (arrayB.filter { it % divider != 0 }.size == arrayB.size) {
+                if (answer < divider) {
+                    answer = divider
                 }
             }
         }
         dividerB.forEach { divider ->
-            if(arrayB.filter { it%divider==0 }.size == arrayB.size){
-                if(arrayA.filter { it%divider!=0 }.size==arrayA.size){
-                    if(answer<divider){
-                        answer = divider
-                    }
+            if (arrayA.filter { it % divider != 0 }.size == arrayA.size) {
+                if (answer < divider) {
+                    answer = divider
                 }
             }
         }
-        println(answer)
         return answer
     }
 }
