4eec05fe610bad17ddecf4ab5af31ece36ca9b0c
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-01-12 15:59:32 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 11.41 ms, Memory: 98.4 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.kt
+++ Kotlin/프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.kt
@@ -1,28 +1,25 @@
-
 class Solution {
     fun solution(sequence: IntArray, k: Int): IntArray {
-
         var answer: IntArray = intArrayOf(0, sequence.size-1)
         var startIndex = 0
         var endIndex = 0
-        var result = sequence[endIndex]
+        var sum = sequence[endIndex]
+
         while (startIndex < sequence.size) {
-            if (result < k) {
-                if (endIndex == sequence.size - 1) break
-                endIndex++
-                result += sequence[endIndex]
-            } else {
-                if (result == k) {
-                    if(answer[1]-answer[0]>endIndex-startIndex){
+            if (sum < k) {
+                if(endIndex == sequence.size-1) break
+                sum += sequence[++endIndex]
+            }
+            else{
+                if(sum == k){
+                    if (answer[1] - answer[0] > endIndex - startIndex) {
                         answer[0] = startIndex
-                        answer[1] = endIndex   
+                        answer[1] = endIndex
                     }
                 }
-                result -= sequence[startIndex]
-                startIndex++
+                sum-=sequence[startIndex++]
             }
         }
-
         return answer
     }
 }
