243e6d389192c383706285c4383758bd26fb2e66
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-01-13 21:52:27 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 179.53 ms, Memory: 110 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.kt
+++ Kotlin/프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.kt
@@ -1,24 +1,21 @@
 class Solution {
     fun solution(elements: IntArray): Int {
         var startIndex = 0
-        var endIndex = 1
-        var sum = elements[startIndex]
+        var endIndex = 0
+        var sum = elements[endIndex]
         val result: MutableSet<Int> = mutableSetOf()
-        while (true) {
-            if (endIndex == elements.size) {
-                endIndex = 0
-            }
-            if (endIndex == startIndex) {
-                result.add(sum)
-                startIndex++
-                if (startIndex > elements.size - 1) break
-                sum = elements[startIndex]
-                endIndex = startIndex + 1
-            } else {
-                result.add(sum)
-                sum += elements[endIndex++]
-            }
+        val size = elements.size
 
+        while (startIndex < size) {
+            result.add(sum)
+            endIndex = (endIndex + 1) % size
+            sum += elements[endIndex]
+
+            if (endIndex == startIndex) {
+                endIndex = (startIndex + 1) % size
+                sum = elements[endIndex]
+                startIndex++
+            }
         }
         return result.size
     }
