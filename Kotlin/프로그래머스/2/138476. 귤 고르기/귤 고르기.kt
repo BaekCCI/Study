849cff19562b3c0736f831062695c7c5e1069f4f
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-01-13 19:36:31 +0900

    [level 2] Title: 귤 고르기, Time: 207.69 ms, Memory: 141 MB -BaekjoonHub

--- Kotlin/프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
+++ Kotlin/프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
@@ -1,18 +1,18 @@
 class Solution {
     fun solution(k: Int, tangerine: IntArray): Int {
         var answer: Int = 0
-        val max = tangerine.maxOrNull() ?: 0
-        var count = IntArray(max + 1)
+        val count = IntArray((tangerine.maxOrNull() ?: 0) + 1)
 
         tangerine.forEach {
             count[it] += 1
         }
         count.sortDescending()
+        
         var sum = 0
-        for(i in count.indices){
-            sum+=count[i]
+        for (i in count.indices) {
+            sum += count[i]
             answer++
-            if(sum>=k)break
+            if (sum >= k) break
         }
         return answer
     }
