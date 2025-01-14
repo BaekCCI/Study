c20c300728b9768cd7176f474a8d7f6bf91baca9
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-01-14 19:17:22 +0900

    [level 3] Title: 단어 변환, Time: 18.27 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/3/43163. 단어 변환/단어 변환.kt
@@ -0,0 +1,35 @@
+
+class Solution {
+    var visit: Array<Boolean> = Array(50) { false }
+    var answer = 50
+
+    fun solution(begin: String, target: String, words: Array<String>): Int {
+
+        if (!words.contains(target)) return 0
+
+        dfs(begin, target, words, 0)
+
+        return answer
+    }
+
+    fun dfs(current: String, target: String, words: Array<String>, depth: Int) {
+        if (current == target) {
+            answer = minOf(answer, depth)
+            return
+        }
+        for (i in words.indices) {
+            if (!visit[i] && canChange(current, words[i])) {
+                visit[i] = true
+                dfs(words[i], target, words, depth + 1)
+                visit[i] = false
+            }
+        }
+    }
+    fun canChange(current: String, word: String): Boolean {
+        var count = 0
+        for (i in current.indices) {
+            if (current[i] != word[i]) count++
+        }
+        return count == 1
+    }
+}
