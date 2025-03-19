7af9d87ee645685ae4c6cf71dbd8b7423a38d58a
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-03-19 20:23:27 +0900

    [level 3] Title: N으로 표현, Time: 27.96 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ Kotlin/프로그래머스/3/42895. N으로 표현/N으로 표현.kt
@@ -0,0 +1,31 @@
+import kotlin.math.*
+
+class Solution {
+    fun solution(N: Int, number: Int): Int {
+        var answer = 0
+        val dp= Array(9){mutableSetOf<Int>()}
+        
+        for(i in 1 until 9){
+            dp[i].add((N.toString().repeat(i)).toInt())
+            
+            for(j in 1 until i){
+                for(dpj in dp[j]){
+                    for(dpji in dp[i-j]){
+                        dp[i].add(dpj+dpji)
+                        dp[i].add(dpj*dpji)
+                        if(dpj - dpji >0){
+                            dp[i].add(abs(dpj-dpji))   
+                        }
+                        if(dpji != 0){
+                            dp[i].add(dpj/dpji)
+                        }
+                    }
+                }
+            }
+            if(dp[i].contains(number)){
+                return i
+            }
+        }
+        return -1
+    }
+}
