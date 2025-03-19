54746829fdcef774b0db9829afe1d00aba4e19b8
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-03-19 20:40:04 +0900

    [level 3] Title: N으로 표현, Time: 23.21 ms, Memory: 67.7 MB -BaekjoonHub

--- Kotlin/프로그래머스/3/42895. N으로 표현/N으로 표현.kt
+++ Kotlin/프로그래머스/3/42895. N으로 표현/N으로 표현.kt
@@ -1,4 +1,4 @@
-import kotlin.math.*
+import kotlin.math.abs
 
 class Solution {
     fun solution(N: Int, number: Int): Int {
@@ -8,16 +8,19 @@ class Solution {
         for(i in 1 until 9){
             dp[i].add((N.toString().repeat(i)).toInt())
             
-            for(j in 1 until i){
+            for(j in 1 .. i/2){
                 for(dpj in dp[j]){
                     for(dpji in dp[i-j]){
                         dp[i].add(dpj+dpji)
                         dp[i].add(dpj*dpji)
-                        if(dpj - dpji >0){
+                        if(dpj - dpji != 0){
                             dp[i].add(abs(dpj-dpji))   
                         }
                         if(dpji != 0){
                             dp[i].add(dpj/dpji)
+                        }
+                        if(dpj != 0){
+                            dp[i].add(dpji/dpj)
                         }
                     }
                 }
