e59d40ffdf7385af1af34f6605110c4f39662364
BaekCCI <133538794+BaekCCI@users.noreply.github.com>
2025-10-11 11:12:30 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 39.30 ms, Memory: 69.7 MB -BaekjoonHub

--- Kotlin/프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
+++ Kotlin/프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
@@ -1,31 +1,28 @@
 class Solution {
     fun solution(today: String, terms: Array<String>, privacies: Array<String>): IntArray {
         var answer: IntArray = intArrayOf()
-
-
-        val todayDays = today.toDays()
-        val formattedTerms: Map<String, Int> = terms.associate {
+        
+        val todayValue = today.toValue()
+        
+        val term : Map<String,Int> = terms.associate{
             val (type, period) = it.split(" ")
             type to period.toInt()
         }
-
-
-        privacies.forEachIndexed { index, privacy ->
-            val (date, type) = privacy.split(" ")
-
-            val expirationDay = date.toDays() + formattedTerms[type]!! * 28 - 1
-
-            if (todayDays > expirationDay) {
-                answer += index + 1
+        
+        for(i in privacies.indices){
+            val (date, type) = privacies[i].split(" ")
+            val dateValue = date.toValue()
+            
+            val periodValue = term[type]!!*28-1
+            if(dateValue+periodValue<todayValue){
+                answer+=i+1
             }
         }
-
         return answer
     }
-
-    fun String.toDays(): Int {
-        val (y, m, d) = this.split(".").map { it.toInt() }
-        return y * 12 * 28 + m * 28 + d
-
+    
+    fun String.toValue():Int{
+        val (y,m,d) = this.split(".").map{it.toInt()}
+        return y*12*28 + m*28 + d
     }
 }
